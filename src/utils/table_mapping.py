6febbb54869ad6151e67ca02e8d1177485ccf824
Raphael Miranda <raphael.miranda@RIOONIBUS.COM>
2024-11-05 10:04:26 -0300

    Initial commit

---
+++ src/utils/table_mapping.py
@@ -0,0 +1,6 @@
+# Define o mapeamento de nomes de tabela
+table_name_mapping = {
+    'EnvioSMTR': 'sppo_gps_conecta',
+    'EnvioViagensRetroativasSMTR': 'sppo_realocacao_conecta',
+    'envioViagensSMTR': 'sppo_viagens_conecta'
+}
